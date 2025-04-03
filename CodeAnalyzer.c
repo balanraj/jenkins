489117939a1674c066ed788a9293ee6e94fcbb4c
balanraj <balanmrsi@gmail.com>
2025-04-03 03:10:09 -0400

    Auto-format: Fixed indentation and applied clang-format

--- CodeAnalyzer.c
+++ CodeAnalyzer.c
@@ -1,9 +1,8 @@
-#include <studio.h>
 #include <string.h>
+#include <studio.h>
 
 void main() {
-          char str[10];
-              strcpy(str, "TooManyCharacterAddedall");
-                print(str);
-
+  char str[10];
+  strcpy(str, "TooManyCharacterAddedall");
+  print(str);
 }
--- code_defetced.c
+++ code_defetced.c
@@ -5,45 +5,44 @@
 #define MAX_SIZE 10
 
 void memory_leak() {
-    char *leak = malloc(100);
-    // Memory is not freed
+  char *leak = malloc(100);
+  // Memory is not freed
 }
 
 void buffer_overflow() {
-    char buffer[MAX_SIZE];
-    for (int i = 0; i <= MAX_SIZE; i++) { // Off-by-one error
-        buffer[i] = 'A';
-    }
+  char buffer[MAX_SIZE];
+  for (int i = 0; i <= MAX_SIZE; i++) { // Off-by-one error
+    buffer[i] = 'A';
+  }
 }
 
 void null_pointer_deref() {
-    char *ptr = NULL;
-    printf("%c\n", ptr[0]); // Null dereference
+  char *ptr = NULL;
+  printf("%c\n", ptr[0]); // Null dereference
 }
 
 void uninitialized_var() {
-    int x;
-    if (x > 0) { // 'x' is uninitialized
-        printf("x is positive\n");
-    }
+  int x;
+  if (x > 0) { // 'x' is uninitialized
+    printf("x is positive\n");
+  }
 }
 
 void unused_var() {
-    int unused = 42; // Unused variable
+  int unused = 42; // Unused variable
 }
 
 void strcpy_overflow() {
-    char dest[5];
-    strcpy(dest, "TooLongStringDetectedAll"); // Buffer overflow
+  char dest[5];
+  strcpy(dest, "TooLongStringDetectedAll"); // Buffer overflow
 }
 
 int main() {
-    memory_leak();
-    buffer_overflow();
-    null_pointer_deref();
-    uninitialized_var();
-    unused_var();
-    strcpy_overflow();
-    return 0;
+  memory_leak();
+  buffer_overflow();
+  null_pointer_deref();
+  uninitialized_var();
+  unused_var();
+  strcpy_overflow();
+  return 0;
 }
-
