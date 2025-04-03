30ce62ed29fe40c8a2acee64faf7874728216380
balanraj <151651923+balanraj@users.noreply.github.com>
2025-04-03 13:58:57 +0530

    Update CodeAnalyzer.c

--- CodeAnalyzer.c
+++ CodeAnalyzer.c
@@ -3,9 +3,11 @@
 
 void main() {
   char str[10];
-  strcpy(str, "TooManyCharacterAddedall");
+            strcpy(str, "TooManyCharacterAddedall");
 
-  print(str);
 
-  strcpy(str, "LongstringAdded");
+
+  
+                      print(str);
+            strcpy(str, "LongstringAdded");
 }
