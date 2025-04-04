fc4389018e9151e89c854c35404114c6fa5e96e7
balanraj <balanmrsi@gmail.com>
2025-04-04 07:12:22 -0400

    Auto-format: Fixed indentation and applied clang-format

--- CodeAnalyzer.c
+++ CodeAnalyzer.c
@@ -3,10 +3,8 @@
 
 void main() {
   char str[10];
-        strcpy(str, "TooManyCharacterAddedall");
+  strcpy(str, "TooManyCharacterAddedall");
 
-
-  
   print(str);
   strcpy(str, "LongstringAdded");
 }
